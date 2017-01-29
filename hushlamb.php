ef3f90482d5ba62b1856df5963290bc0b552913a
Michael Dean <mike-dean1@hotmail.com>
2017-01-29 16:29:26 +0000

    added metadata to releases

--- hushlamb.php
+++ hushlamb.php
@@ -11,22 +11,38 @@
  *
  * @package         Hushlamb
  */
- 
+    
+    /**
+     * ==========================================================
+     * CUSTOM METABOXES
+     * ==========================================================
+     */
     /**
      * Register metaboxes
-     * 
-     * @see https://www.smashingmagazine.com/2011/10/create-custom-post-meta-boxes-wordpress/
      */
+     
+    /**
+     * Store the names of all the meta keys so they can be saved in a single function
+     * @see hushlamb_save_post_class_meta()
+     */
+    
+    $hushlamb_meta_keys = array();
     
     // Hook into Wordpress loading procedure
     add_action( 'add_meta_boxes', 'hushlamb_register_metaboxes' );
     
     function hushlamb_register_metaboxes() {
-        
         add_meta_box( 
             'release-info', 
             __( 'Release Information', 'hushlamb' ), 
             'hushlamb_release_info_fields', 
+            'post'
+        );
+        
+        add_meta_box( 
+            'purchase-links', 
+            __( 'Purchase Links', 'hushlamb' ), 
+            'hushlamb_purchase_links_fields', 
             'post'
         );
     }
@@ -46,49 +62,69 @@
      * @return void
      */
     function hushlamb_release_info_fields( $object ) {
-    
-        wp_nonce_field( basename( __FILE__ ), 'hushlamb_class_nonce' );
         
         /**
-         * Artist
+         * Store the meta keys for each field that is created so they can be saved later
+         * @see hushlamb_save_post_class_meta()
          */
-        ?>
-            <p>
-                <label for="hushlamb-artist"><?php _e( "Artist", 'hushlamb' ); ?></label>
-                <br />
-                <input class="widefat" type="text" name="hushlamb-artist" id="hushlamb-artist" value="<?php echo esc_attr( get_post_meta( $object->ID, 'hushlamb_artist', true ) ); ?>" size="30" />
-            </p>
-        <?php
+        
+        global $hushlamb_meta_keys;
+        $hushlamb_meta_keys[] = 'hushlamb_release_date';
+        $hushlamb_meta_keys[] = 'hushlamb_catalog_number';
+    
+        wp_nonce_field( basename( __FILE__ ), 'hushlamb_post_class_nonce' );
         
         /**
          * Release date and catalog number
         */
         ?>
             <p>
-                <label for="hushlamb-release-date"><?php _e( "Release Date", 'hushlamb' ); ?></label>
-                <br />
-                <input class="widefat" type="text" name="hushlamb-release-date" id="hushlamb-release-date" value="<?php echo esc_attr( get_post_meta( $object->ID, 'hushlamb_release_date', true ) ); ?>" size="30" />
-                
                 <label for="hushlamb-catalog-number"><?php _e( "Catalog No.", 'hushlamb' ); ?></label>
                 <br />
-                <input class="widefat" type="text" name="hushlamb-catalog-number" id="hushlamb-catalog-number" value="<?php echo esc_attr( get_post_meta( $object->ID, 'hushlamb_catalog_number', true ) ); ?>" size="30" />
+                <input class="widefat" type="text" name="hushlamb-catalog-number" id="hushlamb-catalog-number" value="<?php echo esc_attr( get_post_meta( $object->ID, 'hushlamb_catalog_number', true ) ); ?>" size="30" /><label for="hushlamb-release-date"><?php _e( "Release Date", 'hushlamb' ); ?></label>
+                <br />
+                <input class="date-picker" type="date" name="hushlamb-release-date" id="hushlamb-release-date" value="<?php echo esc_attr( get_post_meta( $object->ID, 'hushlamb_release_date', true ) ); ?>" style="display: block;" />
+                
+            </p>
+        <?php
+    }
+    
+        
+    /**
+     * Traxsource and Beatport links
+    */
+    function hushlamb_purchase_links_fields( $object ) {
+        
+        /**
+         * Store the meta keys for each field that is created so they can be saved later
+         * @see hushlamb_save_post_class_meta()
+         */
+        
+        global $hushlamb_meta_keys;
+        
+        $hushlamb_meta_keys[] = 'hushlamb_beatport_link';
+        $hushlamb_meta_keys[] = 'hushlamb_amazon_link';
+        $hushlamb_meta_keys[] = 'hushlamb_traxsource_link';
+    
+        wp_nonce_field( basename( __FILE__ ), 'hushlamb_post_class_nonce' );
+        
+        ?>
+            <p>
+                <label for="hushlamb-beatport-link"><?php _e( "Beatport", 'hushlamb' ); ?></label>
+                <br />
+                <input class="widefat" type="text" name="hushlamb-beatport-link" id="hushlamb-beatport-link" value="<?php echo esc_attr( get_post_meta( $object->ID, 'hushlamb_beatport_link', true ) ); ?>" size="30" />
+                
+                <label for="hushlamb-amazon-link"><?php _e( "Amazon", 'hushlamb' ); ?></label>
+                <br />
+                <input class="widefat" type="text" name="hushlamb-amazon-link" id="hushlamb-amazon-link" value="<?php echo esc_attr( get_post_meta( $object->ID, 'hushlamb_amazon_link', true ) ); ?>" size="30" />
+                
+                <label for="hushlamb-traxsource-link"><?php _e( "Traxsource", 'hushlamb' ); ?></label>
+                <br />
+                <input class="widefat" type="text" name="hushlamb-traxsource-link" id="hushlamb-traxsource-link" value="<?php echo esc_attr( get_post_meta( $object->ID, 'hushlamb_traxsource_link', true ) ); ?>" size="30" />
+
             </p>
         <?php
         
-        /**
-         * Traxxsource and Beatport links
-        */
-        ?>
-            <p>
-                <label for="hushlamb-traxxsource-link"><?php _e( "Traxxsource", 'hushlamb' ); ?></label>
-                <br />
-                <input class="widefat" type="text" name="hushlamb-traxxsource-link" id="hushlamb-traxxsource-link" value="<?php echo esc_attr( get_post_meta( $object->ID, 'hushlamb_traxxsource-link', true ) ); ?>" size="30" />
-                
-                <label for="hushlamb-beatport-link"><?php _e( "Beatport", 'hushlamb' ); ?></label>
-                <br />
-                <input class="widefat" type="text" name="hushlamb-beatport-link" id="hushlamb-beatport-link" value="<?php echo esc_attr( get_post_meta( $object->ID, 'hushlamb_beatport-link', true ) ); ?>" size="30" />
-            </p>
-        <?php
     }
     
     /**
@@ -99,12 +135,24 @@
     add_action( 'save_post', 'hushlamb_save_post_class_meta', 10, 2 );
     
     function hushlamb_save_post_class_meta( $post_id, $post ) {
-         
+        
+        //Save all the metadata by interating through the saved meta keys.
+        //@TODO Fix this so the meta keys don't have to be decaled here
+        //global $hushlamb_meta_keys;
+        
+        $hushlamb_meta_keys = array();
+        
+        $hushlamb_meta_keys[] = 'hushlamb_release_date';
+        $hushlamb_meta_keys[] = 'hushlamb_catalog_number';
+        
+        $hushlamb_meta_keys[] = 'hushlamb_beatport_link';
+        $hushlamb_meta_keys[] = 'hushlamb_amazon_link';
+        $hushlamb_meta_keys[] = 'hushlamb_traxsource_link';
+        
         /* Verify the nonce before proceeding. */
         if ( !isset( $_POST['hushlamb_post_class_nonce'] ) || !wp_verify_nonce( $_POST['hushlamb_post_class_nonce'], basename( __FILE__ ) ) ) {
             return $post_id;
         }
-        
         /* Get the post type object. */
         $post_type = get_post_type_object( $post->post_type );
         
@@ -113,20 +161,16 @@
             return $post_id;
         }
         
-        $meta_keys = array(
-            'hushlamb_artist',
-            'hushlamb_release_date',
-            'hushlamb_catalog_number',
-            'hushlamb_traxxsource-link',
-            'hushlamb_beatport-link'
-        );
-        
-        foreach( $meta_keys as $meta_key ) {
-            
+        //Go through each of the meta keys registered to metaboxes and update them
+        foreach( $hushlamb_meta_keys as $meta_key ) {
             $class_name = str_replace('_', '-', $meta_key);
             
             /* Get the posted data and sanitize it for use as an HTML class. */
-            $new_meta_value = ( isset( $_POST[ $class_name ] ) ? sanitize_html_class( $_POST[ $class_name ] ) : '' );
+            /**
+             * @TODO Sanitize data, but allow hyperlinks to be stored in database
+             */
+            //$new_meta_value = ( isset( $_POST[ $class_name ] ) ? sanitize_html_class( $_POST[ $class_name ] ) : '' );
+            $new_meta_value = $_POST[ $class_name ];
             
             /* Get the meta value of the custom field key. */
             $meta_value = get_post_meta( $post_id, $meta_key, true );
